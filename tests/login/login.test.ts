ed647e1b04311cc20f0ffce8513e09e27393481d
Vibhakar <zaqturey@gmail.com>
2024-07-14 14:25:08 +0200

    Added generic improvements

--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -5,25 +5,22 @@ test.describe.configure({ mode: 'serial' })
 
 test.describe('login form tests', () => {
   test('logging in works with existing account', async ({ page }) => {
-    await page.goto('localhost:8080/login')
+    await page.goto('http://localhost:8080/login')
 
     const existingUser = existingUsers[0]
 
-    await page
-      .locator('#root form div:nth-child(1) > div > input')
-      .pressSequentially(existingUser.email)
+    // Improvement 01 - Using "page.fill" to simplify filling input fields.
+    await page.fill('#root form div:nth-child(1) > div > input', existingUser.email)
+    await page.fill('#root form div:nth-child(2) > div > input', existingUser.password)
 
-    await page
-      .locator('#root form div:nth-child(2) > div > input')
-      .pressSequentially(existingUser.password)
+    // Improvement 02 - Using "await" to ensure promise are awaited and "page.click" for better readability and conciseness.
+    await page.click('form .MuiButton-sizeMedium')
 
-    // Submit button
-    const button = page.locator('form .MuiButton-sizeMedium')
-    // Click on the button
-    button.click()
+    // Improvement 03 - Replaced hardcoded timeout with "page.waitForSelector" to wait for element to be visible.
+    await page.waitForSelector('text=Log out')
+    await expect(page.locator('text=Log out')).toBeVisible()
 
-    // Wait for 1 second until page is fully loaded
-    await page.waitForTimeout(1000)
-    await expect(page.getByText('Log out')).toBeVisible()
+    // Improvement 04 - Ensure the page has navigated to the correct URL
+    await expect(page).toHaveURL('http://localhost:8080/')
   })
 })
